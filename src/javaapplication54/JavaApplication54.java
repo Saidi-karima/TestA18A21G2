6ebe3769379030875769df69f6769bd478f78a55
kahina-git <77074219+kahina-git@users.noreply.github.com>
2021-09-03 10:06:23 -0400

    Ajout mon premier cmmit bienvenus Kahina...!

--- src/javaapplication54/JavaApplication54.java
+++ src/javaapplication54/JavaApplication54.java
@@ -16,6 +16,7 @@ public class JavaApplication54 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Salut Karima je suis là.....");
     }
     
 }
